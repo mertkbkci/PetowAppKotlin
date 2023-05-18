3531589a7cd3f964a2fb88e4b84750f845d7fe64
mertkbkci <103898638+mertkbkci@users.noreply.github.com>
2023-05-18 13:59:25 +0300

    initial commit

--- app/src/main/java/com/camerba/petowappkotlin/Home/HomeActivity.kt
+++ app/src/main/java/com/camerba/petowappkotlin/Home/HomeActivity.kt
@@ -1,11 +1,14 @@
-package com.camerba.petowappkotlin
+package com.camerba.petowappkotlin.Home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.camerba.petowappkotlin.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+
     }
 }
