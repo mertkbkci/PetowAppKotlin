74d6aca895d42edab4b09c6038904c65538fa99c
mertkbkci <103898638+mertkbkci@users.noreply.github.com>
2023-05-19 10:42:00 +0300

    initial commit

--- app/src/main/java/com/camerba/petowappkotlin/utils/BottomnavigationViewHelper.kt
+++ app/src/main/java/com/camerba/petowappkotlin/utils/BottomnavigationViewHelper.kt
@@ -1,57 +1,13 @@
 package com.camerba.petowappkotlin.utils
 
 import android.content.Context
-import android.content.Intent
-import android.view.MenuItem
-import com.camerba.petowappkotlin.Home.HomeActivity
-import com.camerba.petowappkotlin.Profile.ProfileActivity
-import com.camerba.petowappkotlin.R
-import com.camerba.petowappkotlin.Search.SearchActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class BottomnavigationViewHelper {
 
-    companion object{
-        fun setupBottomNavigation(bottomNavigationView: BottomNavigationView){
+    companion object {
+        fun setupNavigation(context: Context, bottomNavigationView: BottomNavigationView){
 
         }
     }
-
-    fun setupNavigation(context: Context,bottomNavigationView: BottomNavigationView){
-        bottomNavigationView.setOnNavigationItemSelectedListener = object : BottomNavigationView.OnNavigationItemSelectedListener{
-            override fun onNavigationItemSelected(item: MenuItem): Boolean
-            {
-                when(item.itemId)
-                {
-
-                  R.id.ic_home ->
-                  {
-                     val intent = Intent(context,HomeActivity::class.java)
-                      context.startActivity(intent)
-                      return true
-                  }
-                    R.id.ic_search ->
-                    {
-                        val intent = Intent(context,SearchActivity::class.java)
-                        context.startActivity(intent)
-                        return true
-                    }
-                  R.id.ic_profile ->
-                  {
-                      val intent = Intent(context,ProfileActivity::class.java)
-                      context.startActivity(intent)
-                      return true
-                  }
-
-                }
-
-            }
-
-
-        }
-
-
-
-    }
-
 }
