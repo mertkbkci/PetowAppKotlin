ca8f4abfd39fe4903a9ddf88697aca5820711508
mertkbkci <103898638+mertkbkci@users.noreply.github.com>
2023-05-19 14:10:51 +0300

    initial commit

--- app/src/main/java/com/camerba/petowappkotlin/Home/HomeActivity.kt
+++ app/src/main/java/com/camerba/petowappkotlin/Home/HomeActivity.kt
@@ -1,8 +1,11 @@
 package com.camerba.petowappkotlin.Home
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.camerba.petowappkotlin.R
+import com.camerba.petowappkotlin.utils.ListHelper
+import io.ak1.BubbleTabBar
 
 
 class HomeActivity : AppCompatActivity() {
@@ -10,10 +13,13 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-
+setupNavigation()
     }
 
+fun setupNavigation(){
 
+    ListHelper.setupNavigation(this , bubbleTabBar)
+}
 
 
 }
--- app/src/main/java/com/camerba/petowappkotlin/utils/ListHelper.kt
+++ app/src/main/java/com/camerba/petowappkotlin/utils/ListHelper.kt
@@ -3,6 +3,8 @@ package com.camerba.petowappkotlin.utils
 
 
 import android.content.Context
+import android.content.Intent
+import com.camerba.petowappkotlin.R
 import io.ak1.BubbleTabBar
 import io.ak1.OnBubbleClickListener
 
@@ -14,7 +16,28 @@ class ListHelper {
 
             bubbleTabBar.addBubbleListener(object : OnBubbleClickListener {
                 override fun onBubbleClick(id: Int) {
+               when(id.toInt){
 
+                   R.id.ic_home ->{
+
+                       val intent:Intent(context, HomeActivity::class.java)
+                       context.startActivity(intent)
+                       return true
+                   }
+                   R.id.ic_search ->{
+
+                       val intent:Intent(context, HomeActivity::class.java)
+                       context.startActivity(intent)
+                       return true
+                   }
+                   R.id.ic_profile ->{
+
+                       val intent:Intent(context, HomeActivity::class.java)
+                       context.startActivity(intent)
+                       return true
+                   }
+
+               }
 
                 }
             })
