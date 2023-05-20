5acffce86f7a3d0ffee0d7cade6f589bdf4a80d8
mertkbkci <103898638+mertkbkci@users.noreply.github.com>
2023-05-20 18:50:16 +0300

    initial commit

--- app/src/main/java/com/camerba/petowappkotlin/Profile/ProfileSettingsActivity.kt
+++ app/src/main/java/com/camerba/petowappkotlin/Profile/ProfileSettingsActivity.kt
@@ -2,6 +2,7 @@ package com.camerba.petowappkotlin.Profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.camerba.petowappkotlin.R
 import com.camerba.petowappkotlin.utils.ListHelper
 import io.ak1.BubbleTabBar
@@ -19,9 +20,19 @@ class ProfileSettingsActivity : AppCompatActivity() {
 
     private fun fragmentNavigations() {
         tvProfiliDuzenleHesapAyarlari.setOnClickListener {
+         profileSettingRoot.visibility=View.GONE
          var transaction=supportFragmentManager.beginTransaction()
             transaction.replace(R.id.profileSettingContainer, ProfileEditFragment())
+            transaction.addToBackStack("EditProfileFragmentEklendi")
             transaction.commit()
+        }
+        tvCikisYap.setOnClickListener{
+            profileSettingRoot.visibility=View.GONE
+            var transaction=supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.profileSettingContainer, SignOutFragment())
+            transaction.addToBackStack("SignOutFragmentEklendi")
+            transaction.commit()
+
         }
     }
 
@@ -29,6 +40,11 @@ class ProfileSettingsActivity : AppCompatActivity() {
         imgBack.setOnClickListener{
             onBackPressed()
         }
+    }
+
+    override fun onBackPressed() {
+        profileSettingRoot.visibility=View.VISIBLE
+        super.onBackPressed()
     }
 
     fun setupNavigationView(){
