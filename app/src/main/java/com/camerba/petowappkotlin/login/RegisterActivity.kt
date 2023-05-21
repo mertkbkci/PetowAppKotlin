5705e69f64a6a413537413a2de8eafc9f1be8382
mertkbkci <103898638+mertkbkci@users.noreply.github.com>
2023-05-21 14:19:37 +0300

    initial commit

--- app/src/main/java/com/camerba/petowappkotlin/login/RegisterActivity.kt
+++ app/src/main/java/com/camerba/petowappkotlin/login/RegisterActivity.kt
@@ -2,11 +2,26 @@ package com.camerba.petowappkotlin.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.camerba.petowappkotlin.R
 
 class RegisterActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        init()
+
     }
+    private fun init(){
+        tvEposta.setOnClickListener {
+
+            viewTelefon.visibility=View.GONE
+            viewEposta.visibility=View.VISIBLE
+
+        }
+    }
+
 }
