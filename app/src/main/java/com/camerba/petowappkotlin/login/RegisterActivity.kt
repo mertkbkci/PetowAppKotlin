84d0572c3e72cdd5d4a04208012f9f9f5adba06c
mertkbkci <103898638+mertkbkci@users.noreply.github.com>
2023-05-21 14:44:30 +0300

    initial commit

--- app/src/main/java/com/camerba/petowappkotlin/login/RegisterActivity.kt
+++ app/src/main/java/com/camerba/petowappkotlin/login/RegisterActivity.kt
@@ -2,7 +2,9 @@ package com.camerba.petowappkotlin.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.text.InputType
+import android.text.TextWatcher
 import android.view.View
 import com.camerba.petowappkotlin.R
 
@@ -33,7 +35,25 @@ class RegisterActivity : AppCompatActivity() {
             etGirisYontemi.inputType=InputType.TYPE_CLASS_NUMBER
             etGirisYontemi.setHint("Telefon")
         }
+        etGirisYontemi.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (count >=10){
+
+                }
+            }
+
+
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+            })
+
+
+
     }
-
-
 }
